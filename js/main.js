f02c0bab839c8eb4d864d02d8cabd4fa756719d6
Matias <matiasrsolis@yahoo.com.ar>
2020-11-18 13:48:14 -0300

    Componente Detalles

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
         $(".iconMenu").toggleClass("none");
         $(".iconCloseHide").toggleClass("iconClose");
         $("#logoHeader").toggleClass("logoSmall");
+        $("#btnMenu").css("border", "none");
     });
     
     $('#myTab a').on('click', function (e) {
